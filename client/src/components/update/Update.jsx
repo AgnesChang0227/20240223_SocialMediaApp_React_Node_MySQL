703b1a27201544cf89d158aaebd4b19d56efbd8b
AgnesChang0227 <109337309+AgnesChang0227@users.noreply.github.com>
2024-03-04 23:15:19 +0800

    update css

--- client/src/components/update/Update.jsx
+++ client/src/components/update/Update.jsx
@@ -7,6 +7,7 @@ import ValidationForm from "../../layout/validationForm";
 import ClearIcon from '@mui/icons-material/Clear';
 import cover from "../../assets/defaultCover.jpg";
 import person from "../../assets/person.png";
+import {Box, Button} from "@mui/material";
 
 const validationSchema = yup.object().shape({
   name: yup.string().required("Required"),
@@ -73,7 +74,7 @@ const Update = ({setOpenUpdate, user}) => {
     <div className="update">
       <div className="card">
         <div className="left">
-          <ClearIcon style={{position:"absolute",top:"60px",right:"60px"}}
+          <ClearIcon style={{position:"absolute",top:"10px",right:"10px"}}
                      onClick={() => setOpenUpdate(false)}/>
           <div className="images">
             <div>
@@ -92,6 +93,19 @@ const Update = ({setOpenUpdate, user}) => {
               />
             </div>
           </div>
+          <Box style={{position:"relative",bottom:"-120px"}}
+            sx={{ display: 'flex', flexDirection: 'row',justifyContent: 'space-between' }}>
+            <div style={{width:"50%",padding:"10px"}}>
+              <h4>coverPic.png</h4>
+              <Button style={{width:"100%"}}>Cover Picture</Button>
+            </div>
+            <div style={{width:"50%",padding:"10px"}}>
+              <h4>profilePic.png</h4>
+              <Button style={{width:"100%"}}>Profile Picture</Button>
+            </div>
+
+          </Box>
+
         </div>
         <div className="right">
           <h1>Update Profile</h1>
